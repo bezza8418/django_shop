0abee683490a54247f4dc767ca938d16b78e0ffa
viktor bezuglov <bezza8418@gmail.com>
2026-05-01 20:30:22 +0500

    - добавлены контроллеры home и contacts, - настроена маршрутизация.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'catalog'
 
 urlpatterns = [
-
+    path('', views.home, name='home'),           # Главная страница
+    path('contacts/', views.contacts, name='contacts'),  # Страница контактов
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """Контроллер для главной страницы"""
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    """Контроллер для страницы контактов"""
+    return render(request, 'catalog/contacts.html')
