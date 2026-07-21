c3214211dc4afec76e0206a1907abed8be72d5df
viktor bezuglov <bezza8418@gmail.com>
2026-07-21 21:14:50 +0500

    добавлена модель BlogPost, миграции, регистрация в админке

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    """Модель блоговой записи"""
+    title = models.CharField(
+        max_length=200,
+        verbose_name='заголовок'
+    )
+    content = models.TextField(
+        verbose_name='содержимое'
+    )
+    preview = models.ImageField(
+        upload_to='blog/previews/',
+        blank=True,
+        null=True,
+        verbose_name='превью (изображение)'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='дата создания'
+    )
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='опубликовано'
+    )
+    views_count = models.PositiveIntegerField(
+        default=0,
+        verbose_name='количество просмотров'
+    )
+
+    class Meta:
+        verbose_name = 'блоговая запись'
+        verbose_name_plural = 'блоговые записи'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
