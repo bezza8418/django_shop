3c2527cd032ac7f22e42c02db003e6bb2f325d70
viktor bezuglov <bezza8418@gmail.com>
2026-08-11 19:10:35 +0500

    добавлено низкоуровневое кеширование списка продуктов в категории (ключ category_{id}, TTL 15 минут)

--- catalog/services.py
+++ catalog/services.py
@@ -1,6 +1,14 @@
+from django.core.cache import cache
 from .models import Product
 
 
 def get_products_by_category(category_id):
-    """Возвращает список продуктов в указанной категории"""
-    return Product.objects.filter(category_id=category_id, status='published')
+    """Возвращает список продуктов в указанной категории с кешированием"""
+    cache_key = f'category_{category_id}'
+    products = cache.get(cache_key)
+
+    if products is None:
+        products = list(Product.objects.filter(category_id=category_id, status='published'))
+        cache.set(cache_key, products, 60 * 15)  # кеш на 15 минут
+
+    return products
